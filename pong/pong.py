6a33874e01a0ac2c9618feeddc43b8b3f9fff322
Daniel Brito <danielbrito@pq.uenf.br>
2023-08-31 22:59:29 -0300

    feat(pong): fix frame rate and present elapsed frames

--- pong/pong.py
+++ pong/pong.py
@@ -14,14 +14,19 @@ class PongUI:
 
         self.black = (0, 0, 0)
         self.white = (255, 255, 255)
+        self.font = pygame.font.Font(None, 36)
+        self.fps = 60
+        self.clock = pygame.time.Clock()
 
         self.game = game
 
     def run(self):
+        frame_count = 0
         done = False
         # self.game.reset()
 
         while not done:
+            frame_count += 1
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -42,15 +47,23 @@ class PongUI:
             self.player_paddle.y = self.game.paddle_y
 
             # Update display
+            
+            elapsed_frames_text = self.font.render(f"Elapsed Frames: {frame_count}", True, (255, 255, 255))
+
             self.screen.fill(self.black)
+
+            self.screen.blit(elapsed_frames_text, (10, 10))
+
             pygame.draw.rect(self.screen, self.white, self.player_paddle)
             pygame.draw.ellipse(self.screen, self.white, self.ball)
             pygame.display.flip()
 
+            self.clock.tick(self.fps)
+
 class PongGame():
     def __init__(self):
         self.ball_speed = (5,5)
-        self.ball_radius = 15
+        self.ball_radius = 30
 
         self.paddle_speed = 10
         self.paddle_width = 15
@@ -94,12 +107,7 @@ if __name__ == "__main__":
     print("test")
     game = PongGame()
     ui = PongUI(game)
-    x = threading.Timer(1,ui.run)
-    x.start()
-     
-    for i in range(50):
-        action = int(input("action: "))
-        game.step(action)
+    ui.run()
 
 
 
