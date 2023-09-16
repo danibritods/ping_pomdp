cb32bdfe3077e09d30079e4fe9fd4679ecbdad46
Daniel Brito <danielbrito@pq.uenf.br>
2023-09-15 22:46:42 -0300

    refactor(main): ingrain model into gridlink

--- pingpomdp/pingpomdp.py
+++ pingpomdp/pingpomdp.py
@@ -9,7 +9,6 @@ class Model:
     def act(self):
         print("model action!")
 
-
 class PongGridlink(gridlink.Gridlink):
     def _env_state_is_undesirable(self, env_state):
         pass
@@ -23,67 +22,17 @@ class PongGridlink(gridlink.Gridlink):
 class PingPOMDP:
     def __init__(self):
         self.env = Pong()
-        self.grid = PongGridlink()
-        self.model = Model()
+        self.grid = PongGridlink(Model())
         #my_agent = Agent( A = A_array, B = B_array, C = C_vector)
 
     def step(self):
         env_state = self.env.state()
-        observation = self.grid.observe(env_state)
-        self.model.observe(observation)
+        self.grid.observe(env_state)
 
-        model_action = self.model.act()
-        env_action = self.grid.act(model_action)
+        env_action = self.grid.act()
         self.env.step(env_action)
-        
-
-        self._apply_protocols(env_state)
-
-
-
 
 if __name__ == "__main__":
     p = PingPOMDP()
 
     p.step()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # def agent_observe_and_act(self, observation): #-> action:
-    #     qs = my_agent.infer_states(observation) # get posterior over hidden states (a multi-factor belief)
-    #     # Do active inference
-    #     q_pi, neg_efe = my_agent.infer_policies() # return the policy posterior and return (negative) expected free energies of each policy as well
-    #     action = my_agent.sample_action()
-    #     return action
-
-        # if self.pong.event == "hit!":
-        #     self.predictable_feedback()
-        # elif self.pong.event == "miss":
-        #     self.unpredictable_feedback()
-        # else:
-        #     observation = grid.observe(pong.state)
-        #     action = agent_observe_and_act(observation)
-        #     pong.step(grid.act(action))
