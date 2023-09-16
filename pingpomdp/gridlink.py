5a84d5296d7c49ca7992ea4f5e80375920fa3057
Daniel Brito <danielbrito@pq.uenf.br>
2023-09-15 22:44:36 -0300

    refactor: gridlink draft

--- pingpomdp/gridlink.py
+++ pingpomdp/gridlink.py
@@ -3,8 +3,9 @@ import numpy as np
 GRID_SHAPE = (8,4)
 
 class Gridlink:
-    def __init__(self):
+    def __init__(self, model):
         self.grid_array = np.zeros(GRID_SHAPE)
+        self.model = model
 
     def read_grid(self, cells_to_read):
         return self.grid_array[cells_to_read]
@@ -16,9 +17,33 @@ class Gridlink:
         self.grid_array[cells_to_write] = values
 
 
-    def observe(self, env_state) -> encoded_observation:
+    def observe(self, env_state):# -> encoded_observation:
+        if self.env_state_is_undesirable(env_state):
+            self._unpredictable_feedback()
+
+        elif self.env_state_is_desirable(env_state):
+            self._predictable_feedback()
+        
+        self._sensory_feedback(env_state)
+
+    def act(self): #-> env_action:
+        self.model.act()
+
+
+    def _env_state_is_undesirable(self, env_state):
         pass
-    
-    def act(self, agent_action) -> env_action:
+
+    def env_state_is_desirable(self, env_state):
         pass
+
+    def _sensory_feedback(env_state):
+        pass
+
+    def _predictable_feedback(self):
+        print("predictable")
+
+    def _unpredictable_feedback(self):
+        print("unpredictable")
+
+
 
