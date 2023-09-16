276f3b577e3ab494cf900cdf18bd5347672fef3a
Daniel Brito <danielbrito@pq.uenf.br>
2023-09-16 16:12:29 -0300

    feat: implement predictable feedback

--- pingpomdp/gridlink.py
+++ pingpomdp/gridlink.py
@@ -1,10 +1,29 @@
 import numpy as np
+GRID_SHAPE = (4,8)
+SENSORY_CELLS = (0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+OBSERVATION_MODE = "whole_grid" #agent can observe "whole_grid" or just "sensory_cells"
+N_PREDICTABLE_CYCLES = 200
+N_UNPREDICTABLE_CYCLES = 400
 
 class Gridlink:
-    def __init__(self, model, grid_shape = (4,8)):
+    def __init__(self, agent, grid_shape = GRID_SHAPE, sensory_cells = SENSORY_CELLS, observation_mode = OBSERVATION_MODE):
+        self.agent = agent
         self.shape = grid_shape
         self.grid_array = np.zeros(grid_shape)
-        self.model = model
+
+        self.sensory_cells = sensory_cells
+
+        if observation_mode == "whole_grid":
+            self.send_observation = self.read_all
+        elif observation_mode == "sensory_cells":
+            self.send_observation = self.read_sensory_cells
+        else:
+            raise ValueError("observation_mode invalid! It can be either 'whole_grid' or 'sensory_cells'")
+        #add the option of sensory AREA instead of only sensory CELLS
+
+        #later will be more efficient use the grid just as a interface, 
+        # without actually updating and reading the grid all the time.
+        #I'll work on it another, less day
 
     def read_cells(self, cells_to_read):
         rows, cols = self._cells_to_rows_cols(cells_to_read)
@@ -12,10 +31,21 @@ class Gridlink:
 
     def read_all(self):
         return self.grid_array
+    
+    def read_sensory_cells(self):
+        rows, cols = self._cells_to_rows_cols(self.sensory_cells)
+        return self.grid_array[rows, cols]
 
     def write(self, cells_to_write, values):
         rows, cols = self._cells_to_rows_cols(cells_to_write)
         self.grid_array[rows, cols] = values
+
+    def write_sensory_cells(self, values):
+        rows, cols = self._cells_to_rows_cols(self.sensory_cells)
+        self.grid_array[rows, cols] = values
+
+    def write_motor_region(self, values):
+        pass
 
     def observe(self, env_state):# -> encoded_observation:
         if self.env_state_is_undesirable(env_state):
@@ -27,7 +57,7 @@ class Gridlink:
         self._sensory_feedback(env_state)
 
     def act(self): #-> env_action:
-        self.model.act()
+       return self.agent.act()
 
 
     def _env_state_is_undesirable(self, env_state):
@@ -40,11 +70,18 @@ class Gridlink:
         pass
 
     def _predictable_feedback(self):
-        print("predictable")
+        for i in range(N_PREDICTABLE_CYCLES):
+            ones = np.ones(len(self.sensory_cells))
+            self.write_sensory_cells(ones)
+            self.agent.observe(self.send_observation())
+
+            zeros = np.zeros(len(self.sensory_cells))
+            self.write_sensory_cells(zeros)
+            self.agent.observe(self.send_observation())
 
     def _unpredictable_feedback(self):
-        print("unpredictable")
-
+        pass
+        
 
     def _oneD_to_twoD(self,index):
         n = self.shape[1]
@@ -61,4 +98,3 @@ class Gridlink:
             # Raise an error if the cells are not valid indexes
             raise ValueError("The cells_index must be either a list of ints or a list of tuples")
         return rows, cols
-
--- pingpomdp/test_gridlink.py
+++ pingpomdp/test_gridlink.py
@@ -2,6 +2,16 @@ import numpy as np
 import numpy.testing as npt
 import gridlink as g
 
+class ModelPatch():
+    def __init__(self):
+        self.observations = []
+
+    def observe(self, observation):
+        obs = observation.copy()
+        self.observations.append(obs)
+    
+
+
 def test_grid_basic():
     test_grid = g.Gridlink(None, (4,8))
     assert test_grid.grid_array.shape == (4,8)
@@ -18,3 +28,14 @@ def test_grid_read():
     exp = np.array([2,3])   
     npt.assert_array_equal(test_grid.read_cells([2,3]), exp)
 
+def test_predictable_feedback():
+    agent = ModelPatch()
+    test_grid = g.Gridlink(agent, (1,4),(1,2), observation_mode="sensory_cells")
+
+    ones = np.ones(len(test_grid.sensory_cells))
+    zeros = np.zeros(len(test_grid.sensory_cells))
+
+    exp_obs = [ones,zeros]*g.N_PREDICTABLE_CYCLES
+    test_grid._predictable_feedback()
+
+    npt.assert_array_equal(agent.observations, exp_obs)
