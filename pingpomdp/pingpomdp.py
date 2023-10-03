96d323e52fd170c6c6f46f4e24f771a79b13945e
Daniel Brito <danielbrito@pq.uenf.br>
2023-10-03 00:26:33 -0300

    refactor: add docstrings to `_active_cell_index`

--- pingpomdp/pingpomdp.py
+++ pingpomdp/pingpomdp.py
@@ -23,7 +23,15 @@ class PongGridlink(Gridlink):
             return False
 
     def _active_cell_index(self, env_state):
-        """Kaggan (2020) encodes game state using paddle relative position to the ball"""
+        """
+        - Kaggan (2020) encodes game state using paddle relative position to the ball
+        - This function emulates their place coding:    
+          - Calculates the distance from paddle center to ball center
+          - Normalize this distance by the screen height
+          - Translate the normalized distance to the number of sensorial cells
+          - Caps the value to be between the possible indexes of n (zero to n - 1)
+        """
+        # TODO: also use proximity
         ball_x = env_state.ball_x
         ball_y = env_state.ball_y
         paddle_Y = env_state.paddle_y
@@ -31,7 +39,7 @@ class PongGridlink(Gridlink):
 
         relative_position = (paddle_Y - ball_y) 
         normalized_rel_pos = round(relative_position / self.env.screen_height * n + n/2)
-        capped_norm_rel_pos = max(0, min(normalized_rel_pos, n))
+        capped_norm_rel_pos = max(0, min(normalized_rel_pos, n - 1))
         
         return capped_norm_rel_pos
 
